344fff9a4d532ffbc4f763355f1fe745c24abaab
Loo Zhang Jing <zzzhangjinggg@gmail.com>
2026-05-05 21:25:23 +0800

    initial commit
    - generate files with the command `npx sv create`, using the 'minimal' template
    - then, in `src/routes/+page.svelte`, add functionality for drawing
      lines on a canvas element using a mouse

---
+++ src/routes/+page.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+	import { onMount } from 'svelte';
+	import Two from 'two.js';
+
+	let canvas: HTMLCanvasElement;
+	let two: Two;
+	onMount(() => {
+		two = new Two({
+			fullscreen: true,
+			domElement: canvas,
+			autostart: true,
+		});
+	});
+
+	let startX = 0;
+	let startY = 0;
+	function drawVector(startX: number, startY: number, endX: number, endY: number) {
+		two.makeLine(startX, startY, endX, endY);
+	}
+	function handleMouseDown(ev: MouseEvent) {
+		startX = ev.clientX;
+		startY = ev.clientY;
+	}
+	function handleMouseUp(ev: MouseEvent) {
+		drawVector(startX, startY, ev.clientX, ev.clientY);
+	}
+</script>
+
+<canvas bind:this={canvas} onmousedown={handleMouseDown} onmouseup={handleMouseUp}></canvas>
