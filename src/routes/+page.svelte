f22810df5698a04f096e1674b460c0733bb8327b
Loo Zhang Jing <zzzhangjinggg@gmail.com>
2026-05-06 14:46:09 +0800

    add class VectorVisualisation

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,6 +3,40 @@
 	import Two from 'two.js';
 	import { Line } from 'two.js/src/shapes/line';
 
+	interface Coordinate {
+		x: number;
+		y: number;
+	}
+
+	class VectorVisualisation {
+		initialPoint = { x: -1, y: -1 };
+		terminalPoint = { x: -1, y: -1 };
+		two: Two;
+		line: Line;
+
+		constructor(initialPoint: Coordinate, terminalPoint: Coordinate, two: Two) {
+			const { x: x1, y: y1 } = initialPoint;
+			const { x: x2, y: y2 } = terminalPoint;
+			this.initialPoint.x = x1;
+			this.initialPoint.y = y1;
+			this.terminalPoint.x = x2;
+			this.terminalPoint.y = y2;
+
+			this.two = two;
+			this.line = this.two.makeLine(x1, y1, x2, y2);
+
+			this.two.update();
+		}
+		changeTerminalPoint(newCoordinates: Coordinate) {
+			const { x, y } = newCoordinates;
+			this.terminalPoint.x = x;
+			this.terminalPoint.y = y;
+			this.line.vertices[1].set(x, y);
+
+			this.two.update();
+		}
+	}
+
 	let canvas: HTMLCanvasElement;
 	let two: Two;
 
@@ -14,27 +48,23 @@
 		});
 	});
 
-	let currentLine: null | Line = null;
+	let currentVector: null | VectorVisualisation = null;
 	function handleMouseDown(ev: MouseEvent) {
 		const x = ev.clientX;
 		const y = ev.clientY;
 
-		currentLine = two.makeLine(x, y, x, y);
-		console.log(currentLine);
-		two.update();
+		currentVector = new VectorVisualisation({ x, y }, { x, y }, two);
 	}
 	function handleMouseMove(ev: MouseEvent) {
-		if (currentLine === null) return;
+		if (currentVector === null) return;
 
-		currentLine.vertices[1].set(ev.clientX, ev.clientY);
-		two.update();
+		currentVector.changeTerminalPoint({ x: ev.clientX, y: ev.clientY });
 	}
 	function handleMouseUp(ev: MouseEvent) {
-		if (currentLine === null) throw Error("`currentLine` is null");
+		if (currentVector === null) throw Error("`currentVector` is null");
 
-		currentLine.vertices[1].set(ev.clientX, ev.clientY);
-		two.update();
-		currentLine = null;
+		currentVector.changeTerminalPoint({ x: ev.clientX, y: ev.clientY });
+		currentVector = null;
 	}
 </script>
 
