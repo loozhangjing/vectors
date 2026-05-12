d72a2dc3e115a4f6f6d38803e59ddad4184fe5ed
Loo Zhang Jing <zzzhangjinggg@gmail.com>
2026-05-12 20:08:45 +0800

    fix wrong vector position when viewport is zoomed
    - solution: translate clientX and clientY into SvelteFlow coordinates
      using screenToFlowPosition()

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,59 +1,12 @@
 <script lang='ts'>
-	import { SvelteFlow, Background, ConnectionMode, type Node, type Edge } from '@xyflow/svelte';
-	import '@xyflow/svelte/dist/style.css';
-
-	import PointNode from '$lib/PointNode.svelte';
-	
-	const nodeTypes = { point: PointNode };
-
-	let nodes: Node[] = $state.raw([]);
-
-	let edges: Edge[] = $state.raw([]);
-
-	let headNodeID: string;
-	function addPoint(x: number, y: number) {
-		// start point names with 'A'
-		const id = String.fromCharCode((nodes.at(-1)?.id || '@').charCodeAt(0) + 1);
-		nodes = nodes.concat({
-			id,
-			type: 'point',
-			position: { x, y },
-			data: { },
-		});
-
-		return id;
-	}
-	function startAddingVector(ev: MouseEvent) {
-		headNodeID = addPoint(ev.clientX, ev.clientY);
-	}
-	function finishAddingVector(ev: MouseEvent) {
-		const tailNodeID = addPoint(ev.clientX, ev.clientY);
-
-		// start point names with 'p'
-		const edgeID = String.fromCharCode((edges.at(-1)?.id || 'o').charCodeAt(0) + 1);
-		edges = edges.concat({
-			id: edgeID,
-			type: 'straight',
-			source: headNodeID,
-			target: tailNodeID
-		});
-	}
+	import { SvelteFlowProvider } from '@xyflow/svelte';
+	import Flow from '$lib/Flow.svelte';
 </script>
 
 <div id='viewport'> 
-	<SvelteFlow
-		bind:nodes
-		bind:edges
-		{nodeTypes}
-		connectionMode={ConnectionMode.Loose}
-		onmousedown={startAddingVector}
-		onmouseup={finishAddingVector}
-		panOnDrag={[1, 2]}
-	>
-		<!-- a value of [1, 2] for panOnDrag allows the viewport to be panned on mouse drag --> 
-		<!-- only when the middle or right mouse buttons are being held down -->
-		<Background />
-	</SvelteFlow>
+	<SvelteFlowProvider>
+		<Flow />
+	</SvelteFlowProvider>
 </div>
 
 <style>
