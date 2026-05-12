7344d43f62942f45287c6c42dbfcdda559e09cc7
Loo Zhang Jing <zzzhangjinggg@gmail.com>
2026-05-12 14:59:57 +0800

    initial commit
    - custom SvelteFlow node for points to represent vector heads/tails

---
+++ src/lib/PointNode.svelte
@@ -0,0 +1,8 @@
+<script lang='ts'>
+	import { Handle, Position, type NodeProps } from '@xyflow/svelte';
+
+	let { id }: NodeProps = $props();
+</script>
+
+<Handle type='source' position={Position.Top} />
+<div><span>{id}</span></div>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,31 @@
+<script>
+	import { SvelteFlow, Background, ConnectionMode } from '@xyflow/svelte';
+	import '@xyflow/svelte/dist/style.css';
+
+	import PointNode from '$lib/PointNode.svelte';
+	
+	const nodeTypes = { point: PointNode };
+
+	let nodes = $state.raw([
+		{ id: 'A', type: 'point', position: { x: 200, y: 0 }, data: { } },
+		{ id: 'B', type: 'point', position: { x: 1000, y: 400 }, data: { } },
+		{ id: 'C', type: 'point', position: { x: 0, y: 200 }, data: { } },
+	]);
+
+	let edges = $state.raw([
+		{ id: 'x', type: 'straight', source: 'A', target: 'B' },
+		{ id: 'y', type: 'straight', source: 'C', target: 'B' },
+		{ id: 'z', type: 'straight', source: 'C', target: 'A' },
+	]);
+</script>
+
+<div style:width="100vw" style:height="100vh">
+	<SvelteFlow
+	 bind:nodes
+	 bind:edges
+	 {nodeTypes}
+	 connectionMode={ConnectionMode.Loose}
+	>
+		<Background />
+	</SvelteFlow>
+</div>
