9aea3bf96e089de96ce6cd2a33893981ad9f2190
Loo Zhang Jing <zzzhangjinggg@gmail.com>
2026-05-13 17:55:52 +0800

    display a '~' underneath vector names
    - and offset the edge so that it starts and ends in the centre of a
      PointNode

--- src/lib/PointNode.svelte
+++ src/lib/PointNode.svelte
@@ -4,12 +4,19 @@
 	let { id }: NodeProps = $props();
 </script>
 
-<Handle type='source' position={Position.Top} />
-<div><span>{id}</span></div>
+<div class='handle-container'>
+	<Handle type='source' position={Position.Top} />
+</div>
+<div class='label-container'><span>{id}</span></div>
 
 <style>
 	span {
-		font-size: 2rem;
+		font-size: 32px;
 		font-family: 'Georgia';
+		font-style: italic;
+	}
+	.handle-container > :global(.svelte-flow__handle) {
+		width: 10px;
+		height: 10px;
 	}
 </style>
--- src/lib/VectorEdge.svelte
+++ src/lib/VectorEdge.svelte
@@ -14,11 +14,30 @@
 </script>
 
 <BaseEdge {id} path={edgePath} />
-<EdgeLabel x={labelX} y={labelY}><span>{id}</span></EdgeLabel>
+<EdgeLabel x={labelX} y={labelY}>
+	<div class='label-container'>
+		<span>{id}</span>
+		<span>~</span>
+	</div>
+</EdgeLabel>
 
 <style>
+	:global(.svelte-flow__edge-path) {
+		/* make the connection start from the centre of a point node instead of from the top */
+		transform: translateY(5px);
+		stroke: black;
+	}
 	span {
-		font-size: 2rem;
+		font-size: 32px;
+		font-family: 'Georgia';
 		font-weight: bold;
+		font-style: italic;
+	}
+	span:last-child {
+		/* force the ~ onto a new line, below the vector name */
+		display: block;
+		position: absolute;
+		left: 0%;
+		top: 60%;
 	}
 </style>
