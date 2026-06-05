fb777990188da8bf234ebca3760aebd44ac27b6f
Loo Zhang Jing <zzzhangjinggg@gmail.com>
2026-06-05 16:14:50 +0800

    find all paths from one point node to another
    - return from the function after reaching a leaf node (previously I just
      pushed every terminated path to the `paths` array), and push the path
      only when the target node has been found
    - added a `NamedVector` type as a substitute for SvelteFlow's
      `NodeConnection` type, because the source node and target node of a
      `NodeConnection` are fixed, and don't necessarily correspond to the head
      node and tail node of the vector in the direction the
      `findPathToNodeRecursively()` function is currently traversing
      - that's also why, in comparison conditions, I check both
      `connection.source` and `connection.target`

--- src/lib/ResultPane.svelte
+++ src/lib/ResultPane.svelte
@@ -1,35 +1,71 @@
 <script lang='ts'>
+	interface NamedVector {
+		name: string,
+		headNodeId: string,
+		tailNodeId: string,
+	}
+
 	import { useSvelteFlow, useNodeConnections } from '@xyflow/svelte';
 	const { getNodes } = useSvelteFlow();
 
-	let sourceNodeId = $state();
-	let tailNodeId = $state();
+	let sourceNodeId = $state('A');
+	let tailNodeId = $state('B');
 
 	$effect(() => {
-		const { current: currentConnections } = useNodeConnections({ id: sourceNodeId });
+		const { current: sourceNodeConnections } = useNodeConnections({ id: sourceNodeId });
 
-		const paths = [];
+		const paths: NamedVector[][] = [];
 
-		for (const connection of currentConnections) {
+		for (const connection of sourceNodeConnections) {
+			// the source and target nodes of a NodeConnection object don't necessarily correspond to
+			// the head and tail nodes of the vector in the direction we're going
 			const nextNodeId = connection.source === sourceNodeId ? connection.target : connection.source;
 
-			findPathToNodeRecursively('', sourceNodeId, nextNodeId, [sourceNodeId, nextNodeId]);
+			const nextPath = [{
+				name: connection.edgeId,
+				headNodeId: sourceNodeId,
+				tailNodeId: nextNodeId,
+			}];
+
+			// if the source node and tail node are connected (share an edge)
+			if (nextNodeId === tailNodeId) {
+				paths.push(nextPath);
+				continue;
+			}
+
+			findPathToNodeRecursively(tailNodeId, nextPath);
 		}
+
 		console.log(paths);
 
-		function findPathToNodeRecursively(targetNodeId: string, previousNodeId: string, currentNodeId: string, currentPath: string[]) {
-			const { current: connections } = useNodeConnections({ id: currentNodeId });
+		function findPathToNodeRecursively(targetNodeId: string, currentPath: NamedVector[]) {
+			const previousVector = currentPath.at(-1);
 
-			if (connections.length <= 1) {
-				paths.push(currentPath);
-				return;
-			}
-			
+			if (!previousVector) throw new Error("`currentPath` is empty");
+
+			const { headNodeId: prevHeadNodeId, tailNodeId: prevTailNodeId } = previousVector;
+			const { current: connections } = useNodeConnections({ id: prevTailNodeId });
+
+			// upon reaching a leaf node (at which point the only direction to go is back), stop traversing
+			if (connections.length <= 1) return;
+
 			for (const connection of connections) {
-				if (connection.source === previousNodeId || connection.target === previousNodeId) continue;
-				const nextNodeId = connection.source === currentNodeId ? connection.target : connection.source;
-				const nextPath = [...currentPath, nextNodeId]
-				findPathToNodeRecursively(targetNodeId, currentNodeId, nextNodeId, nextPath);
+				// don't traverse in the direction we came from
+				if (connection.source === prevHeadNodeId || connection.target === prevHeadNodeId) continue;
+
+				const nextTailNodeId = connection.source === prevTailNodeId ? connection.target : connection.source;
+				const nextVector = {
+					name: connection.edgeId,
+					headNodeId: prevTailNodeId,
+					tailNodeId: nextTailNodeId,
+				};
+				const nextPath = [...currentPath, nextVector]
+
+				if (nextTailNodeId === targetNodeId) {
+					paths.push(nextPath);
+					return;
+				}
+				findPathToNodeRecursively(targetNodeId, nextPath);
 			}
 		}
 	});
