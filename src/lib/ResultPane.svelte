57de74bf8b9eb9e811880bc4fbcbf1c18e4d6e47
Loo Zhang Jing <zzzhangjinggg@gmail.com>
2026-06-05 15:36:47 +0800

    find all paths to leaf nodes
    - starting from the selected sourceNodeId, traverse the node tree until
      a leaf node (terminal node) is found, then add the path (in the form
      of an array of node IDs) to an array [repeat for every path]
      - then log the array of paths to the console

--- src/lib/ResultPane.svelte
+++ src/lib/ResultPane.svelte
@@ -1,14 +1,58 @@
 <script lang='ts'>
-	import { useSvelteFlow } from '@xyflow/svelte';
-	const { getEdges } = useSvelteFlow();
+	import { useSvelteFlow, useNodeConnections } from '@xyflow/svelte';
+	const { getNodes } = useSvelteFlow();
+
+	let sourceNodeId = $state();
+	let tailNodeId = $state();
+
+	$effect(() => {
+		const { current: currentConnections } = useNodeConnections({ id: sourceNodeId });
+
+		const paths = [];
+
+		for (const connection of currentConnections) {
+			const nextNodeId = connection.source === sourceNodeId ? connection.target : connection.source;
+
+			findPathToNodeRecursively('', sourceNodeId, nextNodeId, [sourceNodeId, nextNodeId]);
+		}
+		console.log(paths);
+
+		function findPathToNodeRecursively(targetNodeId: string, previousNodeId: string, currentNodeId: string, currentPath: string[]) {
+			const { current: connections } = useNodeConnections({ id: currentNodeId });
+
+			if (connections.length <= 1) {
+				paths.push(currentPath);
+				return;
+			}
+			
+			for (const connection of connections) {
+				if (connection.source === previousNodeId || connection.target === previousNodeId) continue;
+				const nextNodeId = connection.source === currentNodeId ? connection.target : connection.source;
+				const nextPath = [...currentPath, nextNodeId]
+				findPathToNodeRecursively(targetNodeId, currentNodeId, nextNodeId, nextPath);
+			}
+		}
+	});
 </script>
 
 <div>
-	<h2>Results</h2>
-	{#each getEdges() as edge}
-		<p><span class='squiggly-underline'>{edge.id}</span> = {edge.source}{edge.target}</p>
-	{/each}
+	<label for="sourceNodeSelect">Starting point:</label>
+	<select id="sourceNodeSelect" bind:value={sourceNodeId}>
+		{#each getNodes() as node}
+			<option value={node.id}>{node.id}</option>
+		{/each}
+	</select>
 </div>
 
+<div>
+	<label for="tailNodeSelect">Ending point:</label>
+	<select id="tailNodeSelect" bind:value={tailNodeId}>
+		{#each getNodes() as node}
+			<option value={node.id}>{node.id}</option>
+		{/each}
+	</select>
+</div>
+
+<h2>Results</h2>
 <style>
 </style>
